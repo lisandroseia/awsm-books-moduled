295e00083ee096aca59343ab967ea6f73462a39b
lisandroseia <lisandroseia11@gmail.com>
2022-01-11 13:41:02 -0300

    Fix linters

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import { DateTime } from './node_modules/luxon/src/luxon.js';
 import Book from './modules/book.js';
 import Collection from './modules/collection.js';
 
+import { booksDisp, addDisp, contactDisp } from './modules/navigation.js';
+
 const inputTitle = document.getElementById('title');
 const inputAuthor = document.getElementById('author');
 const submitBtn = document.querySelector('.add-btn');
@@ -24,15 +26,9 @@ const navAdd = document.querySelector('#addNew');
 const navList = document.querySelector('#list');
 const navContact = document.querySelector('#contact');
 
-import { booksDisp } from './modules/navigation.js'
-
 navAdd.addEventListener('click', booksDisp);
 
-import { addDisp } from './modules/navigation.js'
-
-navList.addEventListener('click',addDisp);
-
-import { contactDisp } from './modules/navigation.js'
+navList.addEventListener('click', addDisp);
 
 navContact.addEventListener('click', contactDisp);
 
